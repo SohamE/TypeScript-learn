b7cc5d812a20c626c16e5883abbfbb1c7786f37d
SohamE <soham.584sengupta@gmail.com>
2023-01-26 12:04:20 +0530

    Type Inference

--- Types/variables.ts
+++ Types/variables.ts
@@ -7,3 +7,13 @@ movieTitle.toUpperCase();
 // Not allowed
 // movieTitle = 9;
 // movieTitle.upper();
+
+// Type Inference
+
+let myVariable = "Test";
+
+// by type inference the variable type has been infered to String based on the data.
+
+let myNew;
+// If we dont set any value while defining, the type is set to <any>, which is not advisable, we should
+// add type explicitly if this is the case.
